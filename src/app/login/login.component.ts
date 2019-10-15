75eed2bc7f25cdac06bab5423f7085014606a180
pedrocore73 <pjimenez@corenetworks.es>
2019-10-15 20:58:57 +0200

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { RbacService } from '../servicios/rbac.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,27 +11,34 @@ import { RbacService } from '../servicios/rbac.service';
 export class LoginComponent implements OnInit {
 
   formLogin: FormGroup;
-  logged: boolean;
+  mensaje: string;
+  esperando = false;
 
   constructor(private ff: FormBuilder,
-              private rbacService: RbacService) { }
+              private rbacService: RbacService,
+              private router: Router) { }
 
   ngOnInit() {
     this.formLogin = this.ff.group({
       email: '',
       password: ''
     })
-    this.logged = this.rbacService.isLogged();
-    console.log(this.logged);
   }
 
   sendLogin() {
+    this.esperando = true;
     const auth = {
       email: this.formLogin.get('email').value,
       password: this.formLogin.get('password').value
     }
-    console.log(auth);
-    
+    this.rbacService.login(auth)
+            .subscribe((res: any)=>{
+              this.esperando = false;
+              this.router.navigate(['/inicio']);
+            }, (error: any)=>{
+              this.esperando = false;
+              this.mensaje = error.error.mensaje;
+            })
   }
 
 }
--- src/app/servicios/rbac.service.ts
+++ src/app/servicios/rbac.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RbacService {
 
-  logged = false;
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
-
-  isLogged() {
-    return this.logged;
+  login(credenciales) {
+    return this.http.post('http://localhost:3000/login', credenciales).pipe(
+      map((res: any)=>{
+        console.log(res);
+        return res;
+      })
+    )
   }
 
 }
